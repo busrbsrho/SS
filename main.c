e3394af4891e4f63ee0274c77fefbfb5fd9c832a
busrbsrho <118810462+busrbsrho@users.noreply.github.com>
2022-11-24 23:27:02 +0200

    go go go

--- main.c
+++ main.c
@@ -1,50 +1,58 @@
+#include "NumClass.h"
 #include <stdio.h>
 #include <math.h>
 
-int main()
-{
-	int a= 0;
-		
-	int b= 0;
-	scanf("%d %d", &a, &b);
-	printf("The Armstrong numbers are:");
-	
-	
-	
-	for(int i = a; i <= b; i++)
-	{
-		if(isArmstrong(i) == 1)
-			printf(" %d", i);
-	}
-	
-	printf("\n");
-	
-	
-	printf("The Palindromes are:");
-	for(int i = a; i <= b; i++)
-	{
-		if(isPalindrome(i) == 1)
-			printf(" %d", i);
-	}
-	printf("\n");
-	
-	printf("The Prime numbers are:");
-	
-	for(int i = a; i <= b; i++)
-	{
-		if(isPrime(i) == 1)
-			printf(" %d", i);
-	}
-	printf("\n");
-	printf("The Strong numbers are:");
-	for(int i = a; i <= b; i++)
-	{
-		if(isStrong(i) == 1)
-			printf(" %d", i);
-	}
-	
-	printf("\n");
-	
-	
-	return 0;
+int main(){
+    int a=0;
+    int b=0;
+    scanf("%d%d",&a, &b);
+    printf ("The Armstrong numbers are:");
+         for (int i = a; i <= b; i++)
+    {
+        if (isArmstrong(i) == 1) {
+            printf(" %d", i);
+
+        }
+
+    }
+    printf("\n");
+    printf("The Palindromes are:");
+
+          for (int i = a; i <= b; i++)
+    {
+        if (isPalindrome(i) == 1) {
+                printf(" %d", i);
+
+       	}
+
+    }
+    printf("\n");
+
+
+
+    printf("The Prime numbers are:");
+
+       for (int i = a; i <= b; i++)
+    {
+        if (isPrime(i) == 1) {
+		printf(" %d", i);
+
+        }
+
+    }
+    printf("\n");
+
+    printf("The Strong numbers are:");
+
+        for (int i = a; i <= b; i++)
+    {
+        if (isStrong(i) == 1) {
+            	printf(" %d", i);
+
+        }
+
+    }
+    printf("\n");
+
+    return 0;
 }
