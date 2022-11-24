87d210711f79f037f539bce162706d8b3580fce3
busrbsrho <118810462+busrbsrho@users.noreply.github.com>
2022-11-24 18:39:23 +0200

    commit 5

--- NumClass.h
+++ NumClass.h
@@ -1,4 +1,4 @@
-int isArmstrong(int);
-int isPalindrome(int);
-int isPrime(int);
-int isStrong(int);
+int isArmstrong(int num);
+int isPalindrome(int num);
+int isPrime(int num);
+int isStrong(int num);
