10a4e9f8ef89e5bebf670202dd8fee5608fac3b0
busrbsrho <118810462+busrbsrho@users.noreply.github.com>
2022-11-24 22:18:57 +0200

    fixed is armstrong amen amen

--- advancedClassificationRecursion.c
+++ advancedClassificationRecursion.c
@@ -27,40 +27,31 @@ int isPalindrome(int num) {
 
 
 
-int sumArm(int Number, int len)
+int nLen(int num)
 {
-  int R, Sum = 0;
-  
-  if (Number > 0)
-   {
-     R = Number %10;
-     Sum = Sum + POW(R, len);
-     sumArm (Number /10, len);
-     return Sum;
-   }
-   else
-     return 0;
+    if (num==0)
+    {
+        return 0;
+    }
+    return 1+nLen(num/10);
 }
-
-int lenArm(int num)
+int armHelp(int num,int len)
 {
-int temp=num;
-int count=0;
-while(temp!=0)
-{
-    temp=temp/10;
-    count++;
-}
-return count;
+    if(num==0)
+    {
+        return 0;
+    }
+    return pow(num%10,len)+armHelp(num/10,len);
 }
 int isArmstrong(int num)
 {
-    int len=lenArm(num);
-    if(num==sumArm(num,len))
+    int len = nLen(num);
+    if(num==armHelp(num,len))
     {
         return 1;
     }
     return 0;
 }
+   
 
 
