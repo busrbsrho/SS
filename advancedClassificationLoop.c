26d75fa5c28ca508e484f22841d4518895840660
busrbsrho <118810462+busrbsrho@users.noreply.github.com>
2022-11-24 19:23:58 +0200

    commit added pow recursive

--- advancedClassificationLoop.c
+++ advancedClassificationLoop.c
@@ -2,6 +2,18 @@
 #include <math.h>
 #include "NumClass.h"
 
+int POW(int base, int power)
+{
+    if(power == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return base * POW(base, power - 1);
+    }
+}
+
 int isArmstrong (int num){
     int counter = 0;
     int tempNum = num;
