0eacb952692bd5c5114d81f6d45c66d2f7400c30
busrbsrho <118810462+busrbsrho@users.noreply.github.com>
2022-11-24 18:15:08 +0200

    commit 3

--- basicClassification.c
+++ basicClassification.c
@@ -5,7 +5,15 @@
 
 
 int isPrime(int num){
-    for (int i = 0; i < sqrt(num); i++)
+    if(num <= 0)
+    {
+        return 0;
+    }
+    if(num == 1 || num == 2)
+    {
+        return 1;
+    }
+    for (int i = 2; i < sqrt(num); i++)
     {
         if (num%i==0){
             return 0;
